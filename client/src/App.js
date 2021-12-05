796a367bf0fd94fc5c5d48d9ca31a296b0139678
Jake Joseph <jakejosephcs@gmail.com>
2021-12-05 17:39:38 -0500

    Fix compile error

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [resto, setResto] = useState([]);
-
   useEffect(() => {
     axios
       .get("https://jake-4ww3-project.herokuapp.com/api/restaurants")
