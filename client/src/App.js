c84f4e0154cf8702770559db650a32796e93dbe0
Jake Joseph <jakejosephcs@gmail.com>
2021-12-05 17:55:20 -0500

    Copy app.js over

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,70 @@
-import "./App.css";
-import axios from "axios";
-
-import { useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import SearchPage from "./views/Search/SearchPage";
+import SignupPage from "./views/Signup/SignupPage";
+import LoginPage from "./views/Login/LoginPage";
+import RestaurantPage from "./views/Restaurant/RestaurantPage";
+import ResultsPage from "./views/Results/ResultsPage";
+import SubmissionPage from "./views/Submission/SubmissionPage";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [quertyRating, setQueryRating] = useState(null);
+  const [searchBy, setSearchBy] = useState(null);
+
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    axios
-      .get("https://jake-4ww3-project.herokuapp.com/api/restaurants")
-      .then((res) => console.log(res));
+    setToken(localStorage.getItem("token"));
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <Fragment>
+      <Header token={token} />
+      <Routes>
+        <Route
+          path="/restaurant/:id"
+          element={<RestaurantPage token={token} />}
+        ></Route>
+        <Route
+          path="/results"
+          element={
+            <ResultsPage
+              query={query}
+              setQuery={setQuery}
+              quertyRating={quertyRating}
+              setQueryRating={setQueryRating}
+              searchBy={searchBy}
+              setSearchBy={setSearchBy}
+            />
+          }
+        ></Route>
+        <Route
+          path="/submission"
+          element={<SubmissionPage token={token} />}
+        ></Route>
+        <Route
+          path="/signin"
+          element={<LoginPage setToken={setToken} />}
+        ></Route>
+        <Route path="/signup" element={<SignupPage />}></Route>
+        <Route
+          path="/"
+          element={
+            <SearchPage
+              query={query}
+              setQuery={setQuery}
+              quertyRating={quertyRating}
+              setQueryRating={setQueryRating}
+              searchBy={searchBy}
+              setSearchBy={setSearchBy}
+            />
+          }
+        ></Route>
+      </Routes>
+    </Fragment>
+  );
 }
 
 export default App;
